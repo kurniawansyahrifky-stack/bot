07ee8f8f713c88e77117e419ff13658067fb9b83
kurniawansyahrifky-stack <kurniawansyahrifky@gmail.com>
2026-01-07 04:18:02 +0700

    Implement basic Telegram bot functionality

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
-print("BOT TEST JALAN")
+import os
+from telegram.ext import ApplicationBuilder, CommandHandler
+
+BOT_TOKEN = os.getenv("8247822535:AAFWkMeAQT9x2G14lXQOE_8T5Py4ysWuw18")
+
+async def start(update, context):
+    await update.message.reply_text("BOT HIDUP")
+
+def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    print("BOT JALAN")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
